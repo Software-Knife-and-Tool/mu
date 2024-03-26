ce48f8fd94b01e2540b6eec3b58a4ee30f67a874
James Putnam <putnamjm.design@gmail.com>
2024-03-26 09:05:12 -0400

    #42 - libcore: feature enhancements

--- src/libcore/types/function.rs
+++ src/libcore/types/function.rs
@@ -129,7 +129,11 @@ impl Core for Function {
                         (":lambda".to_string(), format!("{:x}", form.as_u64()))
                     }
                     Type::Keyword => (
-                        ":native".to_string(),
+                        ":core".to_string(),
+                        Vector::as_string(mu, Symbol::name(mu, form)).to_string(),
+                    ),
+                    Type::Vector if Vector::type_of(mu, form) == Type::Char => (
+                        ":namespace".to_string(),
                         Vector::as_string(mu, Symbol::name(mu, form)).to_string(),
                     ),
                     _ => panic!(),
