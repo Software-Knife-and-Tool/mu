83b8ce9ae823014078e4a1ece6d97ac0c7921c62
James M. Putnam <putnamjm.design@gmail.com>
2024-04-21 14:40:28 -0400

    #175 - lib: non-blocking I/O framework (#179)
    * #175 - lib: non-blocking I/O framework
    
    * #175 - lib: non-blocking I/O framework

--- src/libenv/streams/mod.rs
+++ src/libenv/streams/mod.rs
@@ -1,3 +1,8 @@
+//  SPDX-FileCopyrightText: Copyright 2024 James M. Putnam (putnamjm.design@gmail.com)
+//  SPDX-License-Identifier: MIT
+
+//! system streams
+pub mod asyncio;
 pub mod operator;
 pub mod read;
 pub mod system;
