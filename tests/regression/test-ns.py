d911e7d597e2daf28c545a9776ed1f258003771c
James Putnam <putnamjm.design@gmail.com>
2024-09-28 20:06:38 -0400

    #470 - test: add common to regression

--- tests/regression/test-ns.py
+++ tests/regression/test-ns.py
@@ -20,6 +20,15 @@ def runtest(line, group, test, expected):
                                 stdout=subprocess.PIPE,\
                                 stderr=subprocess.PIPE)
 
+    if ns == 'common':
+        proc = subprocess.Popen(['../../dist/mu-sys',
+                                 '-l../../dist/core.l',
+                                 '-l../../dist/common.l',
+                                 '-l./common.l',
+                                 '-e (eval-common-form "' + test + '")'],\
+                                stdout=subprocess.PIPE,\
+                                stderr=subprocess.PIPE)
+
     obtained = proc.stdout.read()[:-1].decode('utf8')
     err = proc.stderr.read()[:-1].decode('utf-8')
 
